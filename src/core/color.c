3ebddb8f89fbd09b38b2f96c184a72ebf1bcfa23
Antoine-Bouclier <135241983+Antoine-Bouclier@users.noreply.github.com>
2025-02-18 14:13:06 +0100

    first step into minilibx

---
+++ src/core/color.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouclie <abouclie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 14:07:06 by abouclie          #+#    #+#             */
+/*   Updated: 2025/02/18 14:11:16 by abouclie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minilibx-linux/mlx.h"
+
+int	create_trgb(unsigned char t, unsigned char r, unsigned char g, unsigned char b)
+{
+	return (*(int *)(unsigned char [4]){b, g, r, t});
+}
+unsigned char	get_t(int trgb)
+{
+	return (((unsigned char *)&trgb)[3]);
+}
+
+unsigned char	get_r(int trgb)
+{
+	return (((unsigned char *)&trgb)[2]);
+}
+
+unsigned char	get_g(int trgb)
+{
+	return (((unsigned char *)&trgb)[1]);
+}
+
+unsigned char	get_b(int trgb)
+{
+	return (((unsigned char *)&trgb)[0]);
+}
